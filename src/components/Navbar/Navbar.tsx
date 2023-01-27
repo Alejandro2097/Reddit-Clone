36b5e319ef8d6c699fea280e63e875b36f802cce
Alejandro2097 <alejo12381@hotmail.es>
2023-01-26 21:31:35 -0500

    reddit images

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 
 type NavbarProps = {
@@ -9,8 +9,9 @@ const Navbar:React.FC<NavbarProps> = () => {
     
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex>
-
+            <Flex align='center'>
+                <Image src="/images/redditFace.svg" alt="reddit image" height='30px' />
+                <Image src="/images/redditText.svg" alt="reddit text" height='46px'/>
             </Flex>
 
         </Flex>
