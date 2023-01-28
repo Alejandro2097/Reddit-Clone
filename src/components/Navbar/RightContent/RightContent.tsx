2360b9536b0c8cd6f8d6b958231bf405f59a5ad5
Alejandro2097 <alejo12381@hotmail.es>
2023-01-27 20:52:10 -0500

    right content

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type RightContentProps = {
+    user: any;
+};
+
+const RightContent:React.FC<RightContentProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default RightContent;
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -10,11 +10,11 @@ type SearchInputProps = {
 
 const SearchInput: React.FC<SearchInputProps> = () => {
   return (
-    <Flex flexGrow={1}>
+    <Flex flexGrow={1} mr={2} align="center">
       <InputGroup>
         <InputLeftElement
           pointerEvents="none"
-          children={<SearchIcon color="gray.300" />}
+          children={<SearchIcon color="gray.400" mb={1} />}
         />
         <Input  placeholder="Phone number" 
                 fontSize='10pt' 
