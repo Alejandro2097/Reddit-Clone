0e21c88ac0f69917d9e767edba9104aa1f61ada3
Alejandro2097 <alejo12381@hotmail.es>
2023-01-29 22:01:37 -0500

    auth inputs

---
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -0,0 +1,23 @@
+import { authModalState } from '@/src/atoms/authModalAtom';
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+
+type AuthInputsProps = {
+    
+};
+
+const AuthInputs:React.FC<AuthInputsProps> = () => {
+    const modalState = useRecoilValue(authModalState)
+    return (
+        <Flex direction='column'
+              align='center'
+              width='100%'
+              mt={4}
+        >   
+            {/* {modalState.view === 'login' && <Login/>}
+            {modalState.view === 'signup' && <SignUp/>} */}
+        </Flex>
+    )
+}
+export default AuthInputs;
