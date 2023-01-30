4ae61152c4fe4eab834155266abb9aaf448bfd6f
Alejandro2097 <alejo12381@hotmail.es>
2023-01-29 21:33:48 -0500

    modal state

--- src/atoms/authModalAtom.ts
+++ src/atoms/authModalAtom.ts
@@ -1,6 +1,15 @@
 import { atom } from "recoil";
- 
+
 export interface AuthModalState {
     open: boolean;
     view: "login" | "signup" | "resetPassword"; 
 }
+const defaultModalState: AuthModalState = {
+    open: false,
+    view: 'login'
+}
+
+export const authModalState = atom<AuthModalState>({
+    key: 'authModalState',
+    default: defaultModalState;
+})
