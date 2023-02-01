c31ce72da7982e82ef747a471ba3755d4669f15d
Alejandro2097 <alejo12381@hotmail.es>
2023-01-31 22:09:56 -0500

    firebase

---
+++ src/Firebase/ClientApp.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXY8OZXNDsUc5A86tShnS9xdmIqHMZ0Ok",
+  authDomain: "reddit-clone-tutorial-7d8ce.firebaseapp.com",
+  projectId: "reddit-clone-tutorial-7d8ce",
+  storageBucket: "reddit-clone-tutorial-7d8ce.appspot.com",
+  messagingSenderId: "460392285868",
+  appId: "1:460392285868:web:5426bf7b348a3a628e8109"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
