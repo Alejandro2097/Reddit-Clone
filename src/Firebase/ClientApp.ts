de290aebabfeb3154185e3da1268d985e32beafd
Alejandro2097 <alejo12381@hotmail.es>
2023-01-31 22:17:06 -0500

    necesary inputs

--- src/Firebase/ClientApp.ts
+++ src/Firebase/ClientApp.ts
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
