b80d460d5b4b39b92b416ec87e06bd7b8c2f7e56
Alejandro2097 <alejo12381@hotmail.es>
2023-02-02 22:09:49 -0500

    google sign in complete

--- src/components/Modal/Auth/OAuthButton.tsx
+++ src/components/Modal/Auth/OAuthButton.tsx
@@ -1,22 +1,28 @@
-import { Button, Flex, Image } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Flex, Image, Text } from '@chakra-ui/react';
+import React, { ReactNode } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
+import { auth } from '../../../Firebase/ClientApp'
+
 type OAuthButtonProps = {
-    
+    children?: ReactNode;
 };
 
 const OAuthButton:React.FC<OAuthButtonProps> = () => {
-    
+    const [signInWithGoogle, user, loading, error] =  useSignInWithGoogle(auth);
     return (
         <Flex direction='column'
               width='100%'
               mb={4}>
-            <Button variant='oauth' mb={2}>
+            <Button variant='oauth'
+                    mb={2} width="100%" 
+                    isLoading={loading} 
+                    onClick={() => signInWithGoogle()}>
                 <Image src="/images/googlelogo.png" alt='google logo' height="20px" mr={4}/>
                 Continue whith Google
             </Button>
             <Button variant='oauth'>Some other provider</Button>
+            {error && <Text>error</Text>}
         </Flex>
     )
 }
