22afd0d0a8ccf1e71fd86ff7f8f2b848cd784bbb
Alejandro2097 <alejo12381@hotmail.es>
2023-02-03 23:14:59 -0500

    menu

---
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -0,0 +1,26 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import React from 'react';
+
+type UserMenuProps = {
+    
+};
+
+const UserMenu:React.FC<UserMenuProps> = () => {
+    
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                Actions
+            </MenuButton>
+            <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+                <MenuItem>Delete</MenuItem>
+                <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+        </Menu>
+    )
+}
+export default UserMenu;
