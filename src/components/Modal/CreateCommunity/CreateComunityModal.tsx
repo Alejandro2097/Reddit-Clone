00e688d69f58a976df7ee618ef42abe81c085fd1
Alejandro2097 <alejo12381@hotmail.es>
2023-02-05 21:51:36 -0500

    styles for the modal

--- src/components/Modal/CreateCommunity/CreateComunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateComunityModal.tsx
@@ -1,5 +1,5 @@
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 type CreateComunityModalProps = {
     open: boolean;
@@ -9,24 +9,49 @@ type CreateComunityModalProps = {
 const CreateComunityModal:React.FC<CreateComunityModalProps> = ({
     open,
     handleClose}) => {
-    
+        const [communityName, setCommunityName] = useState('')
         return (
             <>
         
               <Modal isOpen={open} onClose={handleClose}>
                 <ModalOverlay />
                 <ModalContent>
-                  <ModalHeader>Modal Title</ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody>
-                    There is the modal body
-                  </ModalBody>
-        
+                  <ModalHeader display="flex" 
+                               flexDirection="column"
+                               fontSize={15}
+                               padding={3}
+                    >Create a community</ModalHeader>
+                  <Box pl={3} pr={3}>
+                    <ModalCloseButton />
+                    <ModalBody display='flex'
+                               flexDirection='column'
+                               padding="10px 0px"
+                               border="1px solid red">
+                      <Text fontWeight={600} fontSize={15}>
+                        Name
+                      </Text>
+                      <Text fontSize={11} color='gray.500'>
+                        Community names including capitalization cannot be changed
+                      </Text>
+                      <Text position="relative"
+                            top="28px"
+                            left='10px'
+                            width="20px"
+                            >r/</Text>
+                      <Input position="relative"
+                             value={communityName}
+                             size='sm'
+                             pl="22px"
+                             onChange={() => {}}
+                             />
+                    </ModalBody>
+                  </Box>
+                
                   <ModalFooter>
                     <Button colorScheme='blue' mr={3} onClick={handleClose}>
                       Close
                     </Button>
-                    <Button variant='ghost'>Secondary Action</Button>
+                    <Button variant='ghost'>Create Community</Button>
                   </ModalFooter>
                 </ModalContent>
               </Modal>
