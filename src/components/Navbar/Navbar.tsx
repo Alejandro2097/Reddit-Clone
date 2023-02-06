367780745f6c3e53d5f212a9050c9b59d09f35d0
Alejandro2097 <alejo12381@hotmail.es>
2023-02-05 21:12:30 -0500

    solution to some visual bugs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,15 +14,18 @@ type NavbarProps = {
 const Navbar:React.FC<NavbarProps> = () => {
     const [ user, loading, error ] = useAuthState(auth)
     return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex align='center'>
+        <Flex bg="white" 
+              height="44px"
+              padding="6px 12px" 
+              justify={{md: 'space-between'}}>
+            <Flex align='center' width={{base: '40px'}}>
                 <Image src="/images/redditFace.svg" alt="reddit image" height='30px' />
                 <Image src="/images/redditText.svg"
                        alt="reddit text" 
                        height='46px' 
                        display={{ base: 'none', md: "unset"}}/>
             </Flex>
-            <Directory/>
+            {user && <Directory/>}
             <SearchInput user={user}/>
             <RightContent user={user}/> 
             
