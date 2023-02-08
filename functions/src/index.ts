cd51a36f785948379feac20161217ead65c1b2eb
Alejandro2097 <alejo12381@hotmail.es>
2023-02-07 21:45:00 -0500

    store users on database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
+import * as admin from 'firebase-admin';
 import * as functions from "firebase-functions";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUserDocument = functions.auth
+    .user()
+    .onCreate(async (user) => {
+        db.collection('users')
+          .doc(user.uid)
+          .set(JSON.parse(JSON.stringify(user)));
+    })
