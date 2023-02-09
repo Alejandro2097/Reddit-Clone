c9879e93ed218122e0c83b5e9c7df7b220899bde
Alejandro2097 <alejo12381@hotmail.es>
2023-02-08 23:29:04 -0500

    server side props

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,3 +1,6 @@
+import { fireStore } from '@/src/Firebase/ClientApp';
+import { doc, getDoc } from 'firebase/firestore';
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 
 type CommunityPageProps = {
@@ -7,5 +10,29 @@ type CommunityPageProps = {
 const CommunityPage:React.FC<CommunityPageProps> = () => {
     
     return <div>Community Page</div>
+   
 }
+
+
+async function getServerSideProps(context: GetServerSidePropsContext){
+    // get community data and pass it to client 
+
+    try {
+        const communityDocRef = doc(
+            fireStore, 
+            'communities', 
+            context.query.communityId as string);
+        const communityDoc = await  getDoc(communityDocRef);
+
+        return  {
+            props: {
+                communityData: communityDoc.data(), 
+            }
+
+        }
+    } catch (error) {
+        console.log('getServersideProps error',  error);
+    }
+}    
+
 export default CommunityPage;
