2288d68ee26b2b70bb294fa06a4bfb0073cf5f7a
Alejandro2097 <alejo12381@hotmail.es>
2023-02-10 22:54:23 -0500

    not found

---
+++ src/components/community/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type NotFoundProps = {
+    
+};
+
+const NotFound:React.FC<NotFoundProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default NotFound;
