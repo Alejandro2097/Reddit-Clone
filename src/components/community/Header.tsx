8685df84b31759cc29d023b1b6b368c98b1972fa
Alejandro2097 <alejo12381@hotmail.es>
2023-02-12 00:58:05 -0500

    user effects

--- src/components/community/Header.tsx
+++ src/components/community/Header.tsx
@@ -11,7 +11,9 @@ type HeaderProps = {
 
 const Header:React.FC<HeaderProps> = ({communityData}) => {
     const { communityStateValue, onJoinOrLeaveCommunity  } = useCommunityData();
-    const isJoined = false; // read from  our community snippets
+    const isJoined = !!communityStateValue.mySnippets.find(
+            (item) => item.communityId === communityData.id
+    );
     return (
         <Flex direction='column' width='100%' height='146px'>
             <Box height='50%' bg='blue.400'></Box>
--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { auth, fireStore } from '@/src/Firebase/ClientApp';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
 
 import { Community, communityState } from '../atoms/communityAtoms';
 
 const useCommunityData = () => {
-
+    const [user] = useAuthState(auth);
     const [communityStateValue, setCommunityStateValue] = useRecoilState(communityState);
-
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
     const onJoinOrLeaveCommunity = (communityData: Community, isJoined: boolean) => {
         // is the user sign in
         //if not => open auth modal
@@ -16,10 +20,26 @@ const useCommunityData = () => {
         }
         joinCommunity(communityData)
     }
-    
+    const getMySnippets = async () => {
+        setLoading(true);
+        try {
+            // get the user snippets
+            const snippetDocs = await getDocs(
+                collection(fireStore, `users/${user?.uid}/communitySnippets`)
+            );
+            const snippets = snippetDocs.docs.map(doc => ({ ...doc.data()}));
+            console.log("here are snippets", snippets);
+        } catch (error) {
+            console.log('getMySnippetError', error);
+        }
+    }
+
     const joinCommunity = (communityData: Community) => {}
     const leaveCommunity = (communityid: string) => {}
 
+    useEffect(() => {
+        getMySnippets();
+    }, [user]);
     return {
         // data and functions
         communityStateValue,
