71a58183fd8149536c65de24ce5c66f50530a085
Alejandro2097 <alejo12381@hotmail.es>
2023-02-11 21:55:02 -0500

    more styleing

--- src/components/community/Header.tsx
+++ src/components/community/Header.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex, Icon, Image, Text } from '@chakra-ui/react';
 import React from 'react';
+import { FaReddit } from 'react-icons/fa';
 
 import { Community } from '../../atoms/communityAtoms';
 
@@ -14,7 +15,32 @@ const Header:React.FC<HeaderProps> = ({communityData}) => {
             <Box height='50%' bg='blue.400'></Box>
             <Flex justify='center' bg='white' flexGrow={1}>
                 <Flex width='95%' maxWidth='860px' border="1px solid red">
-
+                    {communityData.imageURL ? (
+                        <Image/>
+                    ) : (
+                        <Icon as={FaReddit} 
+                            fontSize={64} 
+                            position='relative' 
+                            top={-3}
+                            color='blue.500'
+                            border='4px solid white'
+                            borderRadius='50%'
+                        />
+                    )}
+                    <Flex padding='10px 16px'>
+                        <Flex direction='column' mr={6}>
+                            <Text fontWeight={800}
+                                  fontSize="16pt">
+                                {communityData.id}
+                            </Text>
+                            <Text fontWeight={600}
+                                  fontSize="10pt"
+                                  color="gray.400">
+                                r/{communityData.id}
+                            </Text>
+                        </Flex>
+                        <Button>{}</Button>
+                    </Flex>
                 </Flex>
             </Flex>
         </Flex>
