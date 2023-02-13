d049c5ce088198775b6706497aa55037d3e38852
Alejandro2097 <alejo12381@hotmail.es>
2023-02-12 20:25:07 -0500

    im in suffer

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -8,24 +8,21 @@ type PageContentProps = {
 const PageContent:React.FC<PageContentProps> = ({children}) => {
     console.log('Here is children', children)
     return (
-        <Flex justify='center' p="16px 0px" border="1px solid red">
+        <Flex justify='center' p="16px 0px" >
             <Flex width='95%' 
-                  border="1px solid green"
                   justify='center'
                   maxWidth="860px"
             >
                 {/* LHS */}
                 <Flex direction="column" 
                       width={{base: "100%", md: '65%'}} 
-                      mr={{base: 0, md: 6}}
-                      border="1px solid blue">
+                      mr={{base: 0, md: 6}}>
                     {children && children[0 as keyof typeof children]}
                 </Flex>
                 {/* RHS */}
                 <Flex direction='column' 
                       display={{ base: "none", md: "flex"}} 
-                      flexGrow={1}
-                      border="1px solid orange">
+                      flexGrow={1}>
                     {children && children[1 as keyof typeof children]}
                 </Flex>
             </Flex>
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,11 +1,21 @@
+import PageContent from '@/src/components/Layout/PageContent';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 
-type submitProps = {
+const SubmitPostPage:React.FC = () => {
     
-};
-
-const submit:React.FC<submitProps> = () => {
-    
-    return <div>Have a good coding</div>
+    return (
+        <PageContent>
+            <>
+                <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
+                    <Text>Create post</Text>
+                </Box>
+            {/*  New post Form  */}
+            </>
+            <>
+             {/* About */}
+            </>
+        </PageContent>
+    )
 }
-export default submit;
+export default SubmitPostPage;
