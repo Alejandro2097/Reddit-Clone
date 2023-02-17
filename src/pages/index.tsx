9b60802b1e9f2af2bfe3dc1bc18ac209b05a28f2
Alejandro2097 <alejo12381@hotmail.es>
2023-02-16 22:17:40 -0500

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,153 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { NextPage } from "next";
+import usePosts from '@/src/hooks/usePost';
+import useCommunityData from '../hooks/useCommunityData';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, fireStore } from '@/src/Firebase/ClientApp';
+import useState from 'react';
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
 
-const inter = Inter({ subsets: ['latin'] })
+const Home: NextPage = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onSelectPost,
+    onDeletePost,
+    onVote,
+  } = usePosts();
+  const { communityStateValue } = useCommunityData();
 
-export default function Home() {
-  return <div>
-    hello
-  </div>
-}
+  const buildUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      if (communityStateValue.mySnippets.length) {
+        // get posts from users' communities
+        const myCommunityIds = communityStateValue.mySnippets.map(
+          (snippet) => snippet.communityId
+        );
+        const postQuery = query(
+          collection(fireStore, "posts"),
+          where("communityId", "in", myCommunityIds),
+          limit(10)
+        );
+        const postDocs = await getDocs(postQuery);
+        const posts = postDocs.docs.map((doc: any) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPostStateValue((prev) => ({
+          ...prev,
+          posts: posts as Post[],
+        }));
+      } else {
+        buildNoUserHomeFeed();
+      }
+    } catch (error) {
+      console.log("buildUserHomeFeed error", error);
+    }
+    setLoading(false);
+  };
+
+  const buildNoUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+
+      // setPostState
+    } catch (error) {
+      console.log("buildNoUserHomeFeed error", error);
+    }
+    setLoading(false);
+  };
+
+  const getUserPostVotes = async () => {
+    try {
+      const postIds = postStateValue.posts.map((post) => post.id);
+      const postVotesQuery = query(
+        collection(firestore, `users/${user?.uid}/postVotes`),
+        where("postId", "in", postIds)
+      );
+      const postVoteDocs = await getDocs(postVotesQuery);
+      const postVotes = postVoteDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        postVotes: postVotes as PostVote[],
+      }));
+    } catch (error) {
+      console.log("getUserPostVotes error", error);
+    }
+  };
+
+  // useEffects
+  useEffect(() => {
+    if (communityStateValue.snippetsFetched) buildUserHomeFeed();
+  }, [communityStateValue.snippetsFetched]);
+
+  useEffect(() => {
+    if (!user && !loadingUser) buildNoUserHomeFeed();
+  }, [user, loadingUser]);
+
+  useEffect(() => {
+    if (user && postStateValue.posts.length) getUserPostVotes();
+
+    return () => {
+      setPostStateValue((prev) => ({
+        ...prev,
+        postVotes: [],
+      }));
+    };
+  }, [user, postStateValue.posts]);
+
+  return (
+    <PageContent>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                onSelectPost={onSelectPost}
+                onDeletePost={onDeletePost}
+                onVote={onVote}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue
+                }
+                userIsCreator={user?.uid === post.creatorId}
+                homePage
+              />
+            ))}
+          </Stack>
+        )}
+      </>
+      <Stack spacing={5}>
+        <Recommendations />
+        <Premium />
+        <PersonalHome />
+      </Stack>
+    </PageContent>
+  );
+};
+
+export default Home;
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -3,34 +3,35 @@ import PageContent from '@/src/components/Layout/PageContent';
 import NewPostForm from '@/src/components/Post/NewPostForm';
 import { auth } from '@/src/Firebase/ClientApp';
 import useCommunityData from '@/src/hooks/useCommunityData';
-import { Box, Text, } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from "react-firebase-hooks/auth";
 
-const SubmitPostPage:React.FC = () => {
-    const [user] = useAuthState(auth);
-    // const communityStateValue = useRecoilValue(communityState);
-    const { communityStateValue } = useCommunityData();
-    console.log("COMMUNITY", communityStateValue);
-    return (
-        <PageContent>
-          <>
-            <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
-              <Text>Create a post</Text>
-            </Box>
-            {user && (
-              <NewPostForm
-                user={user}
-                communityImageURL={communityStateValue.currentCommunity?.imageURL}
-              />
-            )}
-          </>
-          <>
-            {communityStateValue.currentCommunity && (
-              <About communityData={communityStateValue.currentCommunity} />
-            )}
-          </>
-        </PageContent>
-      );
-}
+const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
+  // const communityStateValue = useRecoilValue(communityState);
+  const { communityStateValue } = useCommunityData();
+  console.log("COMMUNITY", communityStateValue);
+
+  return (
+    <PageContent>
+      <>
+        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text>Create a post</Text>
+        </Box>
+        {user && (
+          <NewPostForm
+            user={user}
+            communityImageURL={communityStateValue.currentCommunity?.imageURL}
+          />
+        )}
+      </>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
+    </PageContent>
+  );
+};
 export default SubmitPostPage;
