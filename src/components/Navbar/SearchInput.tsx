d404adcfecc00e8237877a444171a5bea3c86aa9
Alejandro2097 <alejo12381@hotmail.es>
2023-02-16 23:08:29 -0500

    code smell for the deployment

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -10,28 +10,28 @@ type SearchInputProps = {
 
 const SearchInput: React.FC<SearchInputProps> = ({user}) => {
   return (
-    <Flex flexGrow={1} mr={2} align="center" maxWidth={user ? 'auto' : '600px'}>
+    <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
       <InputGroup>
-        <InputLeftElement
-          pointerEvents="none"
-          children={<SearchIcon color="gray.400" mb={1} />}
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.400" mb={1} />
+        </InputLeftElement>
+        <Input
+          placeholder="Search Reddit"
+          fontSize="10pt"
+          _placeholder={{ color: "gray.500" }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          height="34px"
+          bg="gray.50"
         />
-        <Input  placeholder="Search Reddit" 
-                fontSize='10pt' 
-                _placeholder={{ color: "gray.500"}}
-                _hover={{
-                  bg : 'white',
-                  border: "1px solid",
-                  borderColor: "blue.500",
-                }}
-                _focus={{
-                  outline: 'none',
-                  border: '1px solid',
-                  borderColor: 'blue.500'
-                }}
-                height='34px'
-                bg="gray.50"
-                />
       </InputGroup>
     </Flex>
   );
